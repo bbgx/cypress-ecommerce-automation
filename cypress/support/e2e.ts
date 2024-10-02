f66a2b0e7cdd89d3b403505ab9a7ce357f4fba16
bbgx <bernardo_bugmann@protonmail.com>
2024-10-01 22:42:21 -0300

    ignore uncaught exceptions

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -18,3 +18,8 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
