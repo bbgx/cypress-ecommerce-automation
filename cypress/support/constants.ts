fbf94bbb3c6c6311ee4195c7c0e016548e02c90b
bbgx <bernardo_bugmann@protonmail.com>
2024-10-01 21:56:00 -0300

    add constant for login and navigation

---
+++ cypress/support/constants.ts
@@ -0,0 +1,22 @@
+export const LOGIN_PAGE = {
+  LOGIN_LOGO_TXT: 'Swag Labs',
+};
+
+export const NAVIGATION = {
+  ALL_ITEMS: {
+      HREF: '#',
+      HREF_TXT: 'All Items'
+  },
+  ABOUT: {
+      HREF: 'https://saucelabs.com/',
+      HREF_TXT: 'About'
+  },
+  LOGOUT: {
+      HREF: '#',
+      HREF_TXT: 'Logout'
+  },
+  RESET_APP_STATE: {
+      HREF: '#',
+      HREF_TXT: 'Reset App State'
+  },
+};
