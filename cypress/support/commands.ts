6c519c9b6a115c78b0e8b7d716370dca60ec519c
bbgx <bernardo_bugmann@protonmail.com>
2024-10-01 22:00:01 -0300

    fix component identification

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -18,5 +18,5 @@ Cypress.Commands.add('loginViaUi', (username, password) => {
   cy.getByDataTest('username').type(username);
   cy.getByDataTest('password').type(password);
   cy.getByDataTest('login-button').click();
-  cy.get('.login-logo').should('be.visible').and('have.text', LOGIN_PAGE.LOGIN_LOGO_TXT);
+  cy.get('.app_logo').should('be.visible').and('have.text', LOGIN_PAGE.LOGIN_LOGO_TXT);
 });
