1f5edc39ceedd019f64b727ce099513728c494f3
bbgx <bernardo_bugmann@protonmail.com>
2024-10-04 09:11:40 -0300

    Trying to add allure report to github actions and deploy on gh pages (probably won't work)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress';
+import { allureCypress } from "allure-cypress/reporter";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,9 +7,16 @@ export default defineConfig({
   env: {
     username: process.env.TEST_USERNAME,
     password: process.env.TEST_PASSWORD,
+    allure: true,
   },
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
+    setupNodeEvents(on, config) {
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
+    },
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
     chromeWebSecurity: false,
     experimentalRunAllSpecs: true,
